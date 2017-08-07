3e8f51dde4f94d116fb2080ea7eded36d7b20a65
Mayank Kumar <mayankk2308@gmail.com>
2017-08-07 13:26:34 +0530

    Maintain kext backup and prevent override of newer kexts

--- purge-nvda.sh
+++ purge-nvda.sh
@@ -1,24 +1,33 @@
 #!/bin/sh
 # Script (purge-nvda.sh) by mac_editor @ egpu.io (mayankk2308@gmail.com)
+# Usage:
+# sudo ./purge-nvda.sh -> moves NVDA kexts to prevent NVDA GPU activation
+# sudo ./purge-nvda.sh restore -> moves NVDA kexts back while keeping a backup - does not override newer versions.
 
 operation="$1"
 backup_dir="/Library/Application Support/Purge-NVDA/"
-final_message="No changes made."
+final_message=""
 mkdir -p "$backup_dir"
 
 if [[ "$operation" == "" ]]
 then
-  echo "Moving NVIDIA drivers..."
-  mv /System/Library/Extensions/NVDA*.kext "$backup_dir"
-  final_message="Complete. Power on Mac with eGPU plugged in only."
+  if [[ "$(ls /System/Library/Extensions/ | grep NVDA)" ]]
+  then
+    echo "Moving NVIDIA drivers..."
+    if [[ "$(ls "$backup_dir")" ]]
+    then
+      rm -r "$backup_dir"*
+    fi
+    mv /System/Library/Extensions/NVDA*.kext "$backup_dir"
+    final_message="Complete. Power on Mac with eGPU plugged in only."
+  else
+  final_message="Kexts already moved. No action required."
+  fi
 elif [[ "$operation" == "restore" ]]
 then
-  if [[ "$(ls "$backup_dir")" ]]
-  then
-    echo "Restoring NVIDIA drivers..."
-    mv "$backup_dir"* /System/Library/Extensions/
-    final_message="Complete. Restart to reinstate default High Sierra behavior."
-  fi
+  echo "Restoring NVIDIA drivers..."
+  rsync -r -u "$backup_dir"* /System/Library/Extensions/
+  final_message="Complete. Restart to reinstate default High Sierra behavior."
 fi
 
 echo "Invoke kext caching..."
